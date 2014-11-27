51cac178aeb9e6e631c2e4685058b395a566d8b9
moondevil999 <eng.999z@gmail.com>
2014-11-27 10:40:43 +0800

    database created

---
+++ db/migrate/20141127101628_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.belongs_to :category
+      t.string :title
+      t.string :description
+
+
+      t.timestamps
+    end
+  end
+end
